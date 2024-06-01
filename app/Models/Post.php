77682eb7c2fe66e9564332df6566f5c4157d209e
鷹羽ゆら <yurayingyu@gmail.com>
2024-06-01 13:43:31 +0900

    Add files via upload

---
+++ app/Models/Post.php
@@ -0,0 +1,11 @@
+<?php
+// postsテーブルとよしなにやりとりしてくれます。
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+}
